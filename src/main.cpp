199d94d9e5e6cae4924b597ae14f77ffc1d07343
Xondamir-777 <150456608+Xondamir-777@users.noreply.github.com>
2023-12-03 21:22:22 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,43 @@
 /*
- * Author:
- * Date:
+ * Author: Zokirjonov Xondamir
+ * Date: 3.12.2023
  * Name:
  */
 
 #include <iostream>
 #include "task_1.h"
+#include "task_2.h"
+#include "task_3.h"
+#include "task_4.h"
+#include "task_5.h"
+#include "task_6.h"
+#include "task_7.h"
+#include "task_8.h"
+#include "task_9.h"
+#include "task_10.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    cout << "Task 1" << endl;
+    task1();
+    cout << "Task 2" << endl;
+    task2();
+    cout << "Task 3" << endl;
+    task3();
+    cout << "Task 4" << endl;
+    task4();
+    cout << "Task 5" << endl;
+    task5();
+    cout << "Task 6" << endl;
+    task6();
+    cout << "Task 7" << endl;
+    cout << recursivePow(13, 2) << endl;
+    cout << "Task 8" << endl;
+    task8();
+    cout << "Task 9" << endl;
+    task9();
+    cout << "Task 10" << endl;
+    task10();
     return 0;
 }
